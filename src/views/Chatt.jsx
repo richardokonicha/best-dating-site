b71915e6b6fd041e45f0661a5b96886190618d9a
Richard Okonicha <richardokonicha@gmail.com>
2021-01-27 12:06:31 +0100

    remove amplify

--- src/views/Chatt.jsx
+++ src/views/Chatt.jsx
@@ -6,7 +6,7 @@ const Chat = (props) => {
       <input
         onChange={(event) => setInput("name", event.target.value)}
         style={styles.input}
-        value=''
+        value=""
         placeholder="Name"
       />
       <input
@@ -29,13 +29,31 @@ const Chat = (props) => {
 };
 
 const styles = {
-  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: {  marginBottom: 15 },
-  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
-  todoName: { fontSize: 20, fontWeight: 'bold' },
+  container: {
+    width: 400,
+    margin: "0 auto",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: 20,
+  },
+  todo: { marginBottom: 15 },
+  input: {
+    border: "none",
+    backgroundColor: "#ddd",
+    marginBottom: 10,
+    padding: 8,
+    fontSize: 18,
+  },
+  todoName: { fontSize: 20, fontWeight: "bold" },
   todoDescription: { marginBottom: 0 },
-  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
-}
-
+  button: {
+    backgroundColor: "black",
+    color: "white",
+    outline: "none",
+    fontSize: 18,
+    padding: "12px 0px",
+  },
+};
 
 export default Chat;
